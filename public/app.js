b4f39586eea56c0880f29b0aaf3693db0ab8cb28
MattjCamp <mjcampbell74@gmail.com>
2017-04-11 06:41:26 -0400

    Simplified app.js code

--- public/app.js
+++ public/app.js
@@ -19,48 +19,16 @@ grads.applyObject = function(obj, elem) {
   }
 };
 
-grads.problemView = function(data) {
+grads.dataTableView = function(data) {
   var problemNumber = parseInt(data, 10);
-  var view = $('.templates .problem-view').clone();
-  var problemData = grads.problems[problemNumber - 1]; 
-  var resultFlash = view.find('.result'); 
-
-  function checkAnswer() { 
-    var answer = view.find('.answer').val();
-    var test = problemData.code.replace('__', answer) + '; problem();';
-    return eval(test);
-  }
-
-  function checkAnswerClick() {
-    if (checkAnswer()) {
-      var correctFlash = grads.template('correct-flash'); 
-      correctFlash.find('a').attr('href', '#problem-' + (problemNumber + 1)); 
-      grads.flashElement(resultFlash, correctFlash);
-    } else {
-      grads.flashElement(resultFlash, 'Incorrect!');
-    }
-    return false; 
-  }
-
-  view.find('.check-btn').click(checkAnswerClick); 
-  view.find('.title').text('Problem #' + problemNumber);
-  grads.applyObject(problemData, view);
-
-  if (problemNumber < grads.problems.length) {
-    var buttonItem = grads.template('skip-btn'); 
-    buttonItem.find('a').attr('href', '#problem-' + (problemNumber + 1)); $('.nav-list').append(buttonItem);
+  var view = $('.templates .dataTableView').clone();
   
-    view.bind('removingView', function() {
-        buttonItem.remove();
-      });
-  }
-
   return view;
 }
 
 grads.showView = function(hash) {
   var routes = {
-    '#problem': grads.problemView,
+    '#dataTableView': grads.dataTableView,
     '#': grads.landingView,
     '': grads.landingView
   };
@@ -79,31 +47,8 @@ grads.appOnReady = function() {
   grads.showView(window.location.hash);
 }
 
-grads.flashElement = function(elem, content) { 
-  elem.fadeOut('fast', function() {
-    elem.html(content);
-    elem.fadeIn();
-  });
-}
-
 grads.template = function(name) {
   return $('.templates .' + name).clone();
-}
-
-grads.buildCorrectFlash = function (problemNum) {
-  
-  var correctFlash = grads.template('correct-flash');
-  var link = correctFlash.find('a');
-  
-  if (problemNum < grads.problems.length) {
-    link.attr('href', '#problem-' + (problemNum + 1));
-  } else {
-    link.attr('href', '');
-    link.text("You're Finished!");
-  }
-    
-  return correctFlash;
-
 }
 
 grads.landingView = function() {
