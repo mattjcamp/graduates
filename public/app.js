d01d58aa0394888f237a0d8fccab3e946e2dbe7c
MattjCamp <mjcampbell74@gmail.com>
2017-04-15 08:03:14 -0400

    Got over all view working 32 year trend

--- public/app.js
+++ public/app.js
@@ -2,42 +2,57 @@
 
 var grads = {};
 
-grads.problems = [
-  {
-    description: "What is truth?",
-    code: "function problem() { return __; }"
-  },
-  {
-    description: "Simple Math",
-    code: "function problem() { return 42 === 6 * __; }"
-  }
-];
-
-grads.applyObject = function(obj, elem) {
-  for (var key in obj) {
-    elem.find('[data-name="' + key + '"]').text(obj[key]);
-  }
-};
-
-grads.dataTableView = function(data) {
-  var problemNumber = parseInt(data, 10);
-  var view = $('.templates .dataTableView').clone();
-  
+grads.overviewView = function(data) {
+  var view = $('.templates .overviewView').clone();
   return view;
+}
+
+grads.show_table = function() {
+  var vlSpec = {
+    "data": {
+        "url": "data/grads_us.csv"
+      },
+      "width": 400,
+      "mark": "line",
+      "encoding": {
+          "x": {
+          "timeUnit": "year",
+          "field": "year"
+          },
+          "y": {
+          "aggregate": "sum",
+          "field": "n"
+          }
+      }
+  };
+  var opt = {
+    "mode": "vega-lite",
+    "actions" : false
+  };
+  vega.embed("#vis", vlSpec, opt, function(error, result) {
+    // Callback receiving the View instance and parsed Vega spec
+    // result.view is the View, which resides under the '#vis' element
+
+  });
+
 }
 
 grads.showView = function(hash) {
   var routes = {
-    '#dataTableView': grads.dataTableView,
+    '#overviewView': grads.overviewView,
     '#': grads.landingView,
     '': grads.landingView
   };
   var hashParts = hash.split('-');
   var viewFn = routes[hashParts[0]];
+  
   if (viewFn) {
     grads.triggerEvent('removingView', []); 
     $('.view-container').empty().append(viewFn(hashParts[1]));
   }
+  console.log(hashParts[0]);
+  if (hashParts[0] == "#overviewView")
+    grads.show_table();
 }
 
 grads.appOnReady = function() {
@@ -55,5 +70,6 @@ grads.landingView = function() {
   return grads.template('landing-view');
 }
 
-grads.triggerEvent = function(name, args) { $('.view-container>*').trigger(name, args);
+grads.triggerEvent = function(name, args) { 
+  $('.view-container>*').trigger(name, args);
 }
